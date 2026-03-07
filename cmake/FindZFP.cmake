1d96dcb6cd95f44cb8e10a805f25a77ed5dbaa91
Scot Breitenfeld <brtnfld@hdfgroup.org>
2026-03-06 18:15:39 -0600

    Extract ZFP version from headers in FindZFP.cmake
    When ZFP is built with make, the ZFP_VERSION_MAJOR/MINOR/PATCH
    variables are not set, causing test compilation to fail with
    'invalid suffix "x" on integer constant' because ZFP_LIB_VERSION
    ends up as 0x (empty version components). Parse the version from
    zfp/version.h (ZFP 1.0+) or zfp.h (older) to fix this.

--- cmake/FindZFP.cmake
+++ cmake/FindZFP.cmake
@@ -22,6 +22,10 @@ Result Variables
   Include directories for ZFP headers
 ``ZFP_LIBRARIES``
   Libraries to link against
+``ZFP_VERSION``
+  Version string (e.g. "1.0.0")
+``ZFP_VERSION_MAJOR``, ``ZFP_VERSION_MINOR``, ``ZFP_VERSION_PATCH``, ``ZFP_VERSION_TWEAK``
+  Individual version components
 
 Hints
 ^^^^^
@@ -48,9 +52,56 @@ find_library(ZFP_LIBRARY
   PATH_SUFFIXES lib lib64
 )
 
+# Extract version from headers
+if (ZFP_INCLUDE_DIR)
+  # ZFP 1.0.0+ has version in zfp/version.h; older versions in zfp.h
+  if (EXISTS "${ZFP_INCLUDE_DIR}/zfp/version.h")
+    set(_zfp_version_header "${ZFP_INCLUDE_DIR}/zfp/version.h")
+  elseif (EXISTS "${ZFP_INCLUDE_DIR}/zfp.h")
+    set(_zfp_version_header "${ZFP_INCLUDE_DIR}/zfp.h")
+  endif ()
+
+  if (_zfp_version_header)
+    file(STRINGS "${_zfp_version_header}" _zfp_major
+      REGEX "^#define[ \t]+ZFP_VERSION_MAJOR[ \t]+[0-9]+")
+    file(STRINGS "${_zfp_version_header}" _zfp_minor
+      REGEX "^#define[ \t]+ZFP_VERSION_MINOR[ \t]+[0-9]+")
+    file(STRINGS "${_zfp_version_header}" _zfp_patch
+      REGEX "^#define[ \t]+ZFP_VERSION_PATCH[ \t]+[0-9]+")
+    file(STRINGS "${_zfp_version_header}" _zfp_tweak
+      REGEX "^#define[ \t]+ZFP_VERSION_TWEAK[ \t]+[0-9]+")
+
+    if (_zfp_major)
+      string(REGEX REPLACE ".*ZFP_VERSION_MAJOR[ \t]+([0-9]+).*" "\\1" ZFP_VERSION_MAJOR "${_zfp_major}")
+    endif ()
+    if (_zfp_minor)
+      string(REGEX REPLACE ".*ZFP_VERSION_MINOR[ \t]+([0-9]+).*" "\\1" ZFP_VERSION_MINOR "${_zfp_minor}")
+    endif ()
+    if (_zfp_patch)
+      string(REGEX REPLACE ".*ZFP_VERSION_PATCH[ \t]+([0-9]+).*" "\\1" ZFP_VERSION_PATCH "${_zfp_patch}")
+    endif ()
+    if (_zfp_tweak)
+      string(REGEX REPLACE ".*ZFP_VERSION_TWEAK[ \t]+([0-9]+).*" "\\1" ZFP_VERSION_TWEAK "${_zfp_tweak}")
+    else ()
+      set(ZFP_VERSION_TWEAK 0)
+    endif ()
+
+    if (ZFP_VERSION_MAJOR AND ZFP_VERSION_MINOR AND ZFP_VERSION_PATCH)
+      set(ZFP_VERSION "${ZFP_VERSION_MAJOR}.${ZFP_VERSION_MINOR}.${ZFP_VERSION_PATCH}")
+    endif ()
+
+    unset(_zfp_version_header)
+    unset(_zfp_major)
+    unset(_zfp_minor)
+    unset(_zfp_patch)
+    unset(_zfp_tweak)
+  endif ()
+endif ()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ZFP
   REQUIRED_VARS ZFP_LIBRARY ZFP_INCLUDE_DIR
+  VERSION_VAR ZFP_VERSION
 )
 
 if (ZFP_FOUND)
